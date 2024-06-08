8f80c15fce612f47fbb596c68ff506b0e126c922
ForAeons <w.xy020203@gmail.com>
2024-06-08 10:55:42 +0800

    Update shell alias

--- .config/shell/alias.sh
+++ .config/shell/alias.sh
@@ -5,8 +5,12 @@ alias cd="z"
 alias mv="mv -iv"
 alias rm="rm -vI"
 alias grep="grep --color=always"
+
+alias pn="pnpm"
+
 alias v="$EDITOR"
 alias vim="$EDITOR"
 
 alias cdd="cd ~/dev/"
 alias sshsoc="ssh wxiaoyun@pe113.comp.nus.edu.sg"
+alias vpnsoc="sudo openfortivpn --username=e0702008"
