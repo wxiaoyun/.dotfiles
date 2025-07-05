9dcfdefc8bdcd3b0dbdf7f012bde42d1e01ea1b1
wxiaoyun <w.xy020203@gmail.com>
2025-07-05 22:00:13 +0800

    zim config manager and lazygit

---
+++ .config/shell/zim.sh
@@ -0,0 +1,15 @@
+export ZIM_CONFIG_FILE=~/.config/zimrc
+export ZIM_HOME=~/.zim
+
+# Download zimfw plugin manager if missing.
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+    https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
+
+# Install missing modules and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init
+fi
+
+source ${ZIM_HOME}/init.zsh
