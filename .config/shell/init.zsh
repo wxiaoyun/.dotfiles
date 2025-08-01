5581da724defa3bc5aafdedfe94daf650156539f
wxiaoyun <w.xy020203@gmail.com>
2025-08-01 08:36:15 +0800

    feat: opam

--- .config/shell/init.zsh
+++ .config/shell/init.zsh
@@ -5,3 +5,13 @@ eval "$(zoxide init zsh)"
 eval "$(fnm env --use-on-cd --shell zsh)"
 
 eval "$(rustup completions zsh)"
+
+if type brew &>/dev/null; then
+  FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+fi
+
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '$HOME/.opam/opam-init/init.zsh' ]] || source '$HOME/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
