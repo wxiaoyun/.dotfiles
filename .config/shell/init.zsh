47151a49d6b18408621b0094f044102ebd5b9c23
wxiaoyun <w.xy020203@gmail.com>
2026-08-08 17:30:43 +0800

    chore: remove ocaml stuff

--- .config/shell/init.zsh
+++ .config/shell/init.zsh
@@ -4,11 +4,4 @@ eval "$(zoxide init zsh)"
 
 eval "$(rustup completions zsh)"
 
-# This is useful if you're using opam as it adds:
-#   - the correct directories to the PATH
-#   - auto-completion for the opam binary
-# This section can be safely removed at any time if needed.
-[[ ! -r '$HOME/.opam/opam-init/init.zsh' ]] || source '$HOME/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
-eval $(opam env)
-
 export SSH_AUTH_SOCK="$HOME/.ssh/proton-pass-agent.sock"
